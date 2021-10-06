60538eb9b698f5d8bb369ab8a41046ca51f6725d
Ben Leach <benleach@pm.me>
2021-10-06 12:00:09 -0400

    Post 2021 lecture

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 const c = canvas.getContext('2d')
 
+window.addEventListener('resize', () => {
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+})
+
 const canvas2 = document.getElementById('canvas')
 canvas2.width = window.innerWidth
 canvas2.height = window.innerHeight
@@ -42,28 +47,30 @@ const randomX = () => parseInt(Math.random() * window.innerWidth)
 const randowY = () => parseInt(Math.random() * window.innerHeight)
 const randomVelicity = () => parseInt((Math.random() * 5 + 1) * (Math.random() > .5 ? 1 : -1))
 
-const circles = new Array(100).fill(undefined).map(_ => new Circle(randomX(), randowY(), randomVelicity(), randomVelicity(), 30))
+const circles = new Array(25)
+    .fill(undefined)
+    .map(_ => new Circle(randomX(), randowY(), randomVelicity(), randomVelicity(), 30))
 
-function Rectangle(x, y, dx, dy, radius) {
+function Rectangle(x, y, dx, dy, width) {
     this.x = x
     this.y = y
     this.dx = dx
     this.dy = dy
-    this.radius = radius
+    this.width = width
 
     this.draw = function () {
         c.beginPath()
-        c.arc(this.x, this.y, this.radius, Math.PI * 2, false)
+        c.rect(this.x, this.y, width, width)
         c.strokeStyle = 'red'
         c.stroke()
     }
 
     this.update = function () {
         console.log(new Date())
-        if (this.x + radius > window.innerWidth || this.x - radius < 0) {
+        if (this.x + width > window.innerWidth || this.x - width < 0) {
             this.dx = -this.dx
         }
-        if (this.y + radius > window.innerHeight || this.y - radius < 0) {
+        if (this.y + width > window.innerHeight || this.y - width < 0) {
             this.dy = -this.dy
         }
 
@@ -74,8 +81,7 @@ function Rectangle(x, y, dx, dy, radius) {
     }
 }
 
-const recs = new Array(100).fill(undefined).map(_ => new Rectangle(randomX(), randowY(), randomVelicity(), randomVelicity(), 30))
-
+const recs = new Array(25).fill(undefined).map(_ => new Rectangle(randomX(), randowY(), randomVelicity(), randomVelicity(), 30))
 
 const animate = () => {
     requestAnimationFrame(animate)
